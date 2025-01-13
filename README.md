94d98965be5da01a6cb8ffc63bc333e0fa6dd351
Massieh <massieh.berlioz@gmail.com>
2025-01-13 16:29:11 +0100

    Game commit

---
+++ README.md
@@ -0,0 +1,6 @@
+To run the code :
+```pip install numpy```
+Into :
+```py game.py```
+
+You can get python from here if necessary : https://www.python.org/downloads/
---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+    name='Game_Of_Life',
+    version='1.0',
+    author='Massieh',
+    license='MIT',
+    long_description=open('README.md').read(),)
